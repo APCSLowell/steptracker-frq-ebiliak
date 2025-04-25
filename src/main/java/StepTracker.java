dc5d013f3abce7f6d7d20be02328e3c7d5ffcfc0
ebiliak <ilforoughi@s.sfusd.edu>
2025-04-25 00:28:27 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,27 +1,28 @@
 import java.util.ArrayList;
-public class StepTracker
-{
- private int minsteps, totalDays;
- private int totalsteps, ActiveD;
- public StepTracker(int goal){
-minsteps = goal;
-  totalDays= 0;
-  totalsteps=0;
-  ActiveD=0;
- }
-  public void Addaily(int steps){
-   totalsteps += steps;
-   totalDays++;
-    if(steps>= minsteps){
-    ActiveD++;
-     }
-  }
-  public int activedays(){
-   return ActiveD;
- }
- public double averagestep(){
-  
-  return (double) totalsteps/totalDays;
+public class StepTracker {
+    private int minsteps, totalDays, totalsteps, ActiveD;
 
-} 
+    public StepTracker(int goal) {
+        minsteps = goal;
+        totalDays = 0;
+        totalsteps = 0;
+        ActiveD = 0;
+    }
+
+    public void addDaily(int steps) {
+        totalsteps += steps;
+        totalDays++;
+        if (steps >= minsteps) {
+            ActiveD++;
+        }
+    }
+
+    public int activeDays() {
+        return ActiveD;
+    }
+
+    public double averageStep() {
+        if (totalDays == 0) return 0.0; // Avoid divide-by-zero
+        return (double) totalsteps / totalDays;
+    }
 }

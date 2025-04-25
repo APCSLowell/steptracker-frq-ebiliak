3364796736828f661bfac8733a4f5fc7c81dba7a
ebiliak <ilforoughi@s.sfusd.edu>
2025-04-25 00:11:30 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -3,16 +3,17 @@ public class StepTracker
 {
  private int minsteps, totalDays;
  private int totalsteps, activen;
- public Steptracker(int goal){
+ public  class Steptracker(int goal){
 minsteps = goal;
   totalDays= 0;
   totalSteps=0;
   ActiveD=0;
-  Public void Adddaily(int steps){
+ }
+  Public void class Adddaily(int steps){
    totalsteps += steps;
-   totalDays++
+   totalDays++;
     if(steps>= minsteps){
-    active++
+    active++;
      }
   }
   public class active days{
